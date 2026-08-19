d1b213056e346282e5b37bf5fee93d52891eab2d
WLMDATAWIZARD <wlmdatawizard@gmail.com>
2026-08-19 04:50:49 -0500

    Refactor snowflake connection handling to a seperate module and create raw data samples module

--- src/northstar_pipeline.py
+++ src/northstar_pipeline.py
@@ -1,33 +1,10 @@
-import os
 from pathlib import Path
-import snowflake.connector
-from dotenv import load_dotenv
 import csv
+import snowflake.connector
+from snowflake_utils import get_snowflake_connection
 
-
-load_dotenv()
 
 DATA_DIR = Path(__file__).resolve().parent.parent / "data"
-
-SNOWFLAKE_USER = os.getenv("SNOWFLAKE_USER")
-SNOWFLAKE_PASSWORD = os.getenv("SNOWFLAKE_PASSWORD")
-SNOWFLAKE_ACCOUNT = os.getenv("SNOWFLAKE_ACCOUNT")
-SNOWFLAKE_WAREHOUSE = os.getenv("SNOWFLAKE_WAREHOUSE")
-SNOWFLAKE_DATABASE = os.getenv("SNOWFLAKE_DATABASE")
-SNOWFLAKE_SCHEMA = os.getenv("SNOWFLAKE_SCHEMA")
-SNOWFLAKE_ROLE = os.getenv("SNOWFLAKE_ROLE")
-
-
-def get_snowflake_connection():
-    return snowflake.connector.connect(
-        user=SNOWFLAKE_USER,
-        password=SNOWFLAKE_PASSWORD,
-        account=SNOWFLAKE_ACCOUNT,
-        warehouse=SNOWFLAKE_WAREHOUSE,
-        database=SNOWFLAKE_DATABASE,
-        schema=SNOWFLAKE_SCHEMA,
-        role=SNOWFLAKE_ROLE
-    )
 
 
 def test_connection(cursor):
